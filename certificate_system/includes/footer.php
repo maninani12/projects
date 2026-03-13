4c65cc17a340bc3634dd201aa410e29874f59c33
maninani12 <manikona1981@gmail.com>
2026-03-13 16:55:46 +0530

    Delete certificate_system directory

--- certificate_system/includes/footer.php
+++
@@ -1,8 +0,0 @@
- </div>
-    <footer class="footer">
-        <div class="container">
-            <p>&copy; <?php echo date('Y'); ?> Certificate Generation System. All rights reserved.</p>
-        </div>
-    </footer>
-</body>
-</html>
